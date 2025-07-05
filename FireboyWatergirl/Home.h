4d029977053f97280e1f3633e1594d73a081514d
Kaynã Mesquita <mesquitakayna@gmail.com>
2025-07-05 17:49:50 -0300

    Adiciona botões de jogar e sair na tela inicial

--- FireboyWatergirl/Home.h
+++ FireboyWatergirl/Home.h
@@ -1,19 +1,26 @@
 #pragma once
 #include "Game.h"
+#include "Button.h"
 #include "Sprite.h"
 #include "TileSet.h"
 #include "Animation.h"
-#include "Game.h" 
+#include "Font.h" 
 #include "Scene.h"
 #include "Player.h"
 #include "Background.h"
 #include "Level.h"
+#include "Mouse.h"
 
 class Home : public Game
 {
 private:
     Scene * scene = nullptr;
     Background * backg = nullptr;
+    Font * font = nullptr;
+
+    Mouse * mouse;
+    Button * play_button;
+    Button * quit_button;
 
 public:
     Home() = default;
